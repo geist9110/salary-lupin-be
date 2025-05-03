c95288e0f9af65c70dda90af7f3dd3933cc312f4
geist9110 <geist9110@naver.com>
2025-05-04 01:54:53 +0900

    feat: MagicBookAnswerEntity를 무작위로 하나 가지고 오는 기능 구현

--- src/main/java/com/salarylupin/server/magicbook/MagicBookAnswerRepository.java
+++ src/main/java/com/salarylupin/server/magicbook/MagicBookAnswerRepository.java
@@ -1,9 +1,12 @@
 package com.salarylupin.server.magicbook;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MagicBookAnswerRepository extends JpaRepository<MagicBookAnswerEntity, Integer> {
 
+    @Query(value = "SELECT * FROM magic_book_answer ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    MagicBookAnswerEntity findRandomAnswer();
 }
--- src/main/java/com/salarylupin/server/magicbook/MagicBookService.java
+++ src/main/java/com/salarylupin/server/magicbook/MagicBookService.java
@@ -1,11 +1,15 @@
 package com.salarylupin.server.magicbook;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MagicBookService {
 
+    private final MagicBookAnswerRepository magicBookAnswerRepository;
+
     public MagicBookResponseDTO getRandomAnswer() {
-        return new MagicBookResponseDTO("Random answer");
+        return new MagicBookResponseDTO(magicBookAnswerRepository.findRandomAnswer().answer);
     }
 }

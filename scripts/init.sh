503e1b673f8f61de02eee417bcfed3a37808ab59
geist9110 <geist9110@naver.com>
2025-05-13 10:29:35 +0900

    fix: java 설치가 제대로 안되는 버그 수정

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "[init.sh] Checking if Java 21 is installed..."
-if ! java -version 2>&1 | grep -q "21"; then
+
+JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d'.' -f1)
+
+if [ "$JAVA_VERSION" != "21" ]; then
   echo "[init.sh] Java 21 not found. Installing..."
   dnf install -y java-21-amazon-corretto --nogpgcheck
 else
-  echo "[init.sh] Java 21 already installed."
+  echo "[init.sh] Java 21 is already installed."
 fi
 
 APP_DIR=/home/ec2-user/app

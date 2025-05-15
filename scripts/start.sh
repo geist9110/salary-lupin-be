071fcd86aa5a76bc69663f2fd6d186957a6e2e1a
Lee Hyungi <63250720+geist9110@users.noreply.github.com>
2025-05-15 17:16:01 +0900

    EC2 환경의 인프라 세팅 (#7)
    * feat: buildspec에서 docker제거
    
    * feat: codeDeploy를 위한 appspec 추가
    
    * feat: appsepc을 artifact에 추가
    
    * feat: healthcheck를 위한 actuator 추가
    
    * fix: start.sh의 sudo 권한 추가
    
    * feat: init.sh 파일을 통한 로그 파일 저장 권한 추가
    
    * feat: init시 Java21 설치
    
    * feat: 인스턴스의 os를 amazonlinux2023으로 변경에 따른 쉘 스크립트 변경
    
    * feat: 변수값을 AWS에서 직접 가져오도록 변경
    
    * refactor: start.sh에서 instance id를 가져오는 부분을 분리
    
    * fix: IMDSv2 required 버전으로 변경
    
    * fix: 파라미터 내의 오타 수정
    
    * feat: init.sh의 로그 기록용 출력 추가
    
    * fix: java 설치가 제대로 안되는 버그 수정
    
    * feat: url, username, password를 확인하기 위한 로그 추가
    
    * feat: 서버의 포트를 8080으로 변경

---
+++ scripts/start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+APP_DIR=/home/ec2-user/app
+LOG_DIR=$APP_DIR/logs
+JAR_PATH=$APP_DIR/app.jar
+
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
+
+ENV=$(aws ec2 describe-tags \
+  --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=env" \
+  --region ap-northeast-2 \
+  --query "Tags[0].Value" \
+  --output text)
+
+echo "ENVIRONMENT: $ENV"
+
+PARAM_PREFIX="/salarylupin/${ENV}"
+
+DB_URL=$(aws ssm get-parameter \
+  --name "$PARAM_PREFIX/DB_URL" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text)
+
+DB_SECRET_ARN=$(aws ssm get-parameter \
+  --name "$PARAM_PREFIX/DB_SECRET_ARN" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text)
+
+SECRET_JSON=$(aws secretsmanager get-secret-value \
+  --secret-id "$DB_SECRET_ARN" \
+  --query SecretString \
+  --output text)
+
+DB_USERNAME=$(echo "$SECRET_JSON" | jq -r .username)
+DB_PASSWORD=$(echo "$SECRET_JSON" | jq -r .password)
+
+echo "DB_URL=$DB_URL"
+echo "DB_USERNAME=$DB_USERNAME"
+echo "DB_PASSWORD=$DB_PASSWORD"
+
+nohup java \
+  -Dspring.profiles.active=prod \
+  -DDB_URL=$DB_URL \
+  -DDB_USERNAME=$DB_USERNAME \
+  -DDB_PASSWORD=$DB_PASSWORD \
+  -jar $JAR_PATH > $LOG_DIR/app.log 2>&1 &
